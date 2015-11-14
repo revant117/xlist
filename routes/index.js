f254df387e773965e049426549ff87a511866798
revant117 <revantjha.94@gmail.com>
2015-11-14 11:23:21 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {user : req.user });
+});
+
+router.get('/register',function(req,res){
+	res.render('register',{});
+});
+
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { info : "sorry error has occured try fucking again"});
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+
+// another way to this 
+// app.post('/login', passport.authenticate('local', { successRedirect: '/',
+//                                                     failureRedirect: '/login'  ,   failureFlash: true}));    
+//res.redirect('/users/' + req.user.username);  req.user has the user
+//
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+});
+
+module.exports = router;
